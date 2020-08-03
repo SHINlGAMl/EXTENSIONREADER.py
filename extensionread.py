17e11a880352b8d73d6ef1befbacb8fe63f16f04
SHINlGAMl <shawnjacob98@gmail.com>
2020-08-03 10:57:58 +0400

    CORRECTED THE ERROR OF CODE
    COMPLETED THE CODE BY USING ENDSWITH() FOR CHECKING THE EXTENSION.

--- extensionread.py
+++ extensionread.py
@@ -1,12 +1,16 @@
 user_input = input("\nENTER THE FILENAME TO CHECK EXTENSION : ")
-if user_input == *.py
+
+if user_input.lower().endswith('.py') :
     print("\nTHE EXTENSION OF THE FILE IS 'PYTHON'.")
 
-elif user_input == *.jpg
-    print("\nTHE EXTENSION OF THE FILE IS 'IMAGE'")
+elif user_input.lower().endswith(('.png', '.jpg', '.jpng')):
+    print("\nTHE EXTENSION OF THE FILE IS 'IMAGE'.")
 
-elif user_input == *.xct
-    print("\nTHE EXTENSION OF THE FILE IS 'GIMP FILE'")
+elif user_input.lower().endswith('.html'):
+    print("\nTHE EXTENSION OF THE FILE IS 'HTML'.")
 
-elif user_input == *
+elif user_input.lower().endswith('.rtf'):
+    print("\nTHE EXTENSION OF THE FILE IS 'WORD DOCUMENT'.")
 
+else:
+    print("\nSORRY, FAILED TO RECOGNIZE THE EXTENSION OF THE FILENAME INPUTED.")
