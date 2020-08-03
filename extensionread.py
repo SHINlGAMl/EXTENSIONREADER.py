25732e09676cd6a501e996297b1d8cda1b13b878
SHINlGAMl <shawnjacob98@gmail.com>
2020-08-03 10:19:59 +0400

    Update extensionread.py

--- extensionread.py
+++ extensionread.py
@@ -1,2 +1,12 @@
+user_input = input("\nENTER THE FILENAME TO CHECK EXTENSION : ")
+if user_input == *.py
+    print("\nTHE EXTENSION OF THE FILE IS 'PYTHON'.")
+
+elif user_input == *.jpg
+    print("\nTHE EXTENSION OF THE FILE IS 'IMAGE'")
+
+elif user_input == *.xct
+    print("\nTHE EXTENSION OF THE FILE IS 'GIMP FILE'")
+
 elif user_input == *
 
